2a14689b93da140e297951051d1e68087986cc01
Mark Mariscal <markamariscal@gmail.com>
2020-09-10 00:03:23 -0700

    media query update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Cube } from 'react-preloaders';
+import { Cube } from 'react-preloaders';
 import Nav from './components/Nav';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
@@ -9,11 +9,11 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div>
-      {/* <Cube
+      <Cube
         animation="slide-down"
         time={1800}
         background="linear-gradient(180deg, #32292f 0%, #99e1d9 100%)"
-      /> */}
+      />
 
       <Nav />
       <Home />
