1c8230b691daf7b5cace6e481dce24aae414c5a1
Mark Mariscal <markamariscal@gmail.com>
2020-09-11 21:02:30 -0700

    uncommented cube

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-// import { Cube } from 'react-preloaders';
+import { Cube } from 'react-preloaders';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
 import Nav from './components/Nav';
 import Home from './pages/Home';
@@ -15,11 +14,11 @@ library.add(fab, faCheckSquare, faCoffee);
 function App() {
   return (
     <div>
-      {/* <Cube
+      <Cube
         animation="slide-down"
         time={950}
         background="linear-gradient(180deg, #32292f 0%, #99e1d9 100%)"
-      /> */}
+      />
 
       <Nav />
       <Home />
