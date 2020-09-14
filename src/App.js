69133258c7a62dfd0b34264735a9937782aaae1b
Mark Mariscal <markamariscal@gmail.com>
2020-09-14 01:07:28 -0700

    took out cube

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Cube } from 'react-preloaders';
+// import { Cube } from 'react-preloaders';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
@@ -14,11 +14,11 @@ library.add(fab, faCheckSquare, faCoffee);
 function App() {
   return (
     <div>
-      <Cube
+      {/* <Cube
         animation="slide-down"
         time={900}
         background="linear-gradient(180deg, #32292f 0%, #99e1d9 100%)"
-      />
+      /> */}
 
       <Nav />
       <Home />
